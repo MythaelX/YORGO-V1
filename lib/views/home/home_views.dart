635bda3e45eaf4abb65d9e9a43a09e162988984b
MythaelX <chadaut@hotmail.fr>
2021-05-28 16:33:24 +0200

    petite modif

--- lib/views/home/home_views.dart
+++ lib/views/home/home_views.dart
@@ -1,46 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_options.dart';
 
-class HomeView extends StatelessWidget {
+final List<String> imgList = [
+  'assets/images/jogging.jpg',
+  'assets/images/escalade.jpg',
+  'assets/images/swimmer.jpg',
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            child: ClipRRect(
+                child: Stack(
+              children: <Widget>[
+                Image.asset(
+                  item,
+                  fit: BoxFit.cover,
+                  height: 1000,
+                ),
+                Positioned(
+                  top: 50.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'No. ${imgList.indexOf(item)} image',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 50.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'No. ${imgList.indexOf(item)} image',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          ),
+        ))
+    .toList();
+
+class HomeView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeViewState();
+  }
+}
+
+class HomeViewState extends State<HomeView> {
   static String routeName = '/home';
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/jogging.jpg"),
-            fit: BoxFit.cover,
+      body: Column(children: [
+        CarouselSlider(
+          items: imageSliders,
+          options: CarouselOptions(
+              height: height - 50,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Positioned(
+          bottom: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.map((url) {
+              int index = imgList.indexOf(url);
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              );
+            }).toList(),
           ),
         ),
-        padding: EdgeInsets.all(20),
-        alignment: Alignment.center,
-        //color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Bienvenue sur YORGO',
-              style: TextStyle(
-                fontSize: 37,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                shadows: [
-                  Shadow(
-                    color: Colors.black.withOpacity(1),
-                    offset: Offset(3, 2),
-                    blurRadius: 5,
-                  ),
-                ],
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-            ),
-          ],
-        ),
-      ),
+      ]),
     );
   }
 }
